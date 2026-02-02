e6ef2efbca95b81c5d1c48d4e8089d73a84cb709
Brittany Pollock <brittpollock15@gmail.com>
2026-02-02 13:12:08 -0500

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Brittany Pollock on 2/2/26.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
